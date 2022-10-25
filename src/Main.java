356823518934e1f0a1dbf3abcd4e5812f56968ba
Rajitha <rajitha_jayanetti@yahoo.com>
2022-10-25 22:51:58 +0530

    add

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public  static void main(String[] args) throws Exception {
         String rootPath = "C:\\Users\\hp\\Desktop\\test\\";
 
-        XCriticalTextAnalyzer txtAnalyser = new XCriticalTextAnalyzer(rootPath+"test.java");
+        CriticalTextAnalyzer txtAnalyser = new CriticalTextAnalyzer(rootPath+"test.java");
 
         txtAnalyser.startTextAnalyzer();
         //System.out.println(txtAnalyser.getNumTokens());
